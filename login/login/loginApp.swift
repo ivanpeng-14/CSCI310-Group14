0866efbfe2103a9d8cddfc5f71c686c808d93262
ipeng@usc.edu <ipeng@usc.edu>
2021-03-20 23:04:53 -0700

    1st commit, UI done, firebase next

---
+++ login/login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Ivan Peng on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ login2/login2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  login2
+//
+//  Created by Ivan Peng on 3/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
