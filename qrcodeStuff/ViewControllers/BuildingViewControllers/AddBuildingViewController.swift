4c325803eabe42a6b2382b87aa85d18cb4696717
Nick Chen <nchen.dev@gmail.com>
2021-03-28 19:41:34 -0700

    add building function now checks if building with given buildingname already exists, and lets the user know via a label whether success/fail

--- qrcodeStuff/ViewControllers/BuildingViewControllers/AddBuildingViewController.swift
+++ qrcodeStuff/ViewControllers/BuildingViewControllers/AddBuildingViewController.swift
@@ -17,32 +17,61 @@ class AddBuildingViewController: UIViewController, UIDocumentPickerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpElements()
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet weak var errorLabel1: UILabel!
+
+    @IBOutlet weak var errorLabel2: UILabel!
+
     //MARK: Properties
 
     @IBOutlet weak var buildingNameTextField: UITextField!
     
     @IBOutlet weak var capacityTextField: UITextField!
     
+    func setUpElements() {
+        errorLabel1.alpha = 0;
+        errorLabel2.alpha = 0;
+    }
     
     @IBAction func addBuildingButton(_ sender: Any) {
         let db = Firestore.firestore()
-        db.collection("buildings").addDocument(data: [
-            "buildingName": buildingNameTextField.text ,
-            "totalCapacity": Int(capacityTextField.text ?? "0"),
-            "currentCapacity": 0,
-            "currentStudents" : []
-            
-        ]) { err in
-            if let err = err {
-                print("Error writing document: \(err)")
+        var alreadyExists = false;
+        db.collection("buildings").whereField("buildingName", isEqualTo: buildingNameTextField.text).getDocuments(){(querySnapshot, err) in
+            if ((querySnapshot?.isEmpty) == false) {
+                // building already exist
+                self.errorLabel1.text = "Building Already Exists";
+                self.errorLabel1.textColor = UIColor.red;
+                self.errorLabel1.alpha = 1;
             } else {
-                print("Document successfully written!")
+                // building doesn't already exist
+                self.errorLabel1.alpha = 0;
+                db.collection("buildings").addDocument(data: [
+                    "buildingName": self.buildingNameTextField.text ,
+                    "totalCapacity": Int(self.capacityTextField.text ?? "0"),
+                    "currentCapacity": 0,
+                    "currentStudents" : []
+                    
+                ]) { err in
+                    if let err = err {
+                        print("Error writing document: \(err)")
+                        self.errorLabel1.text = "Error writing document: \(err)"
+                        self.errorLabel1.textColor = UIColor.red;
+                    } else {
+                        print("Document successfully written!")
+                        self.errorLabel1.text = "Building successfully added!"
+                        self.errorLabel1.textColor = UIColor.green;
+                        self.buildingNameTextField.text = "";
+                        self.capacityTextField.text = "";
+                    }
+                    self.errorLabel1.alpha = 1;
+                }
             }
-        }
+        };
+        
+        
     }
     
     @IBAction func uploadCSVButton(_ sender: UIButton) {
