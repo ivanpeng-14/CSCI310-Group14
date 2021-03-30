c54e52fcc8317a6e417f310b262184e28f8d7140
jhu25229 <jhu25229@usc.edu>
2021-03-29 17:46:22 -0700

    rip trying again

--- appUITests/qrUITests.swift
+++ appUITests/qrUITests.swift
@@ -42,7 +42,7 @@ class qrUITests: XCTestCase {
          passwordSecureTextField.typeText(password)
 
 
-        app/*@START_MENU_TOKEN@*/.staticTexts["Paste"]/*[[".menus",".menuItems[\"Paste\"].staticTexts[\"Paste\"]",".staticTexts[\"Paste\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
+        //app/*@START_MENU_TOKEN@*/.staticTexts["Paste"]/*[[".menus",".menuItems[\"Paste\"].staticTexts[\"Paste\"]",".staticTexts[\"Paste\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
 
         // passwordSecureTextField.setText(text: password, application: app)
 
